34d7caebdb56738e296f93073e433a6640c73698
iprowriter <obinnamartin72@gmail.com>
2022-12-12 23:00:07 +0100

    First commit of this project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header'
+
+
+function App() {
+  return (
+    <>
+    <Header />
+     <p>one task per day</p>
+    </>
+  );
+}
+
+export default App;
