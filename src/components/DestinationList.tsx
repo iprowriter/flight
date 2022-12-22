4ce9ab3cdc155fc4431d42b3f0ecfa7549f69cba
iprowriter <obinnamartin72@gmail.com>
2022-12-22 20:50:31 +0100

    passing data from child to parent

--- src/components/DestinationList.tsx
+++ src/components/DestinationList.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
@@ -10,8 +11,7 @@ const StyledContainer = styled(Container)`
   padding: 2rem;
 `;
 
-
-export default function DestinationList() {
+export default function DestinationList(props: any) {
   let country = [
     "Germany",
     "Australia",
@@ -34,15 +34,25 @@ export default function DestinationList() {
     "North Korea",
   ];
 
+  const handleSelectedCountry = (e: any) => {
+    const value = e.target.value;
+    console.log(value);
+    props.getCountry(value);
+  };
+
   return (
     <StyledContainer>
       <Paper elevation={2}>
         <Box>
           <List>
             <Grid container direction="row">
-              {country.map((item) => (
-                <Grid item xs={6} md={4} lg={4}>
-                  <ListItem> <Button>{item}</Button> </ListItem>
+              {country.map((item, index) => (
+                <Grid item xs={6} md={4} lg={4} key={index}>
+                  <ListItem>
+                    <Button onClick={handleSelectedCountry} value={item}>
+                      {item}
+                    </Button>
+                  </ListItem>
                 </Grid>
               ))}
             </Grid>
--- src/components/SearchDestination.tsx
+++ src/components/SearchDestination.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import { styled } from '@mui/material/styles';
-import DestinationList from './DestinationList';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import DestinationList from "./DestinationList";
 
 const StyledBox = styled(Box)`
   background-color: aliceblue;
@@ -12,28 +12,46 @@ const StyledBox = styled(Box)`
   text-align: center;
 `;
 
-
-
 export default function SearchDestination() {
-  const [focus, setFocus] = React.useState(false)
+  const [focus, setFocus] = React.useState(false);
+  const [country, setCountry] = React.useState("");
 
-  
+  const getSelectedCountry = (value: string) => {
+    console.log(value, "from");
+  };
+
   return (
     <StyledBox>
       <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '45ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="filled-basic" label="Depart From" variant="filled" onFocus={() => setFocus(true)} />
-      <TextField id="filled-basic" label="Arrive At" variant="filled" onFocus={() => setFocus(true)}/>
-      <Button variant="contained" sx={{height: '7ch'}} onClick={() => setFocus(false)} >Search Flights</Button>
-    </Box>
-    {focus &&  <DestinationList/>
-}
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "45ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="filled-basic"
+          label="Depart From"
+          variant="filled"
+          onFocus={() => setFocus(true)}
+          value={country}
+        />
+        <TextField
+          id="filled-basic"
+          label="Arrive At"
+          variant="filled"
+          onFocus={() => setFocus(true)}
+        />
+        <Button
+          variant="contained"
+          sx={{ height: "7ch" }}
+          onClick={() => setFocus(false)}
+        >
+          Search Flights
+        </Button>
+      </Box>
+      {focus && <DestinationList getCountry={getSelectedCountry} />}
     </StyledBox>
   );
 }
