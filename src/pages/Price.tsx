98d713317707fd38cfa2060dbecc467415c7b7c7
iprowriter <obinnamartin72@gmail.com>
2023-01-03 20:53:33 +0100

    user capture form #8

--- src/pages/Price.tsx
+++ src/pages/Price.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Price1 from "../components/pricingPage_components/Price";
+import UserForm from "../components/pricingPage_components/UserForm";
 
 export default function Price() {
 
     return(
         <>
         <Price1/>
+        <UserForm/>
         </>
     )
 }
