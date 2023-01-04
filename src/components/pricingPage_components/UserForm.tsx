4c6e95c6ebc54f80dbc68e3c40535d075b905263
iprowriter <obinnamartin72@gmail.com>
2023-01-04 19:08:07 +0100

    userform

--- src/components/pricingPage_components/UserForm.tsx
+++ src/components/pricingPage_components/UserForm.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Grid } from "@mui/material";
+import { Button, Container, Grid } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+
+
+const StyledContainer = styled(Container)`
+  margin-top: 20px;
+  padding: 2rem;
+`;
+
+
 
 export default function userForm() {
   return (
-    <Box
+    <StyledContainer>
+        <Paper elevation={2} sx={{margin: "40px"}} >
+
+            <Grid container
+            justifyContent="center"
+            alignItems="center"
+            sx={{padding: "40px"}}
+            >
+
+<Box
       sx={{
         width: 500,
         maxWidth: "100%",
@@ -30,7 +49,14 @@ export default function userForm() {
         <Grid item sx={{width: "100%" }}>
         <TextField fullWidth label="address" id="fullWidth" />
         </Grid>
+        <Grid item sx={{width: "100%" }}>
+        <Button variant="contained" color="info" fullWidth>Continue to Checkout</Button>
+        </Grid>
       </Grid>
     </Box>
+
+            </Grid>
+        </Paper>
+    </StyledContainer>
   );
 }
