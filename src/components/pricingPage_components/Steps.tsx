d05ca4594716e38647684a9be08a5772721a9900
iprowriter <obinnamartin72@gmail.com>
2023-01-08 09:25:28 +0100

    updated stepper

--- src/components/pricingPage_components/Steps.tsx
+++ src/components/pricingPage_components/Steps.tsx
@@ -5,29 +5,45 @@ import { Button, Container } from "@mui/material";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+import Price from "./Price";
+import UserForm from './UserForm';
+import Checkout from "./Checkout";
+import React from "react";
 
 const StyledContainer = styled(Container)`
   margin-top: 20px;
   padding: 2rem;
 `;
 
-const steps = [
-  "Select master blaster campaign settings",
-  "Create an ad group",
-  "Create an ad",
-];
 
 export default function Steps() {
+    //shows which step the user is on
+    const steps = [
+        "Select a Suitable Flight Package",
+        "Provide Your Details",
+        "Make Your Payment",
+      ];
+  
+   
+      //shows the components to be rendered   
+   const stepsComponent = [<Price/>, <UserForm/>, <Checkout/>]
+
+   //controls the steps to be rendered
+   const [activeStep, setActiveStep] = React.useState<number>(2)
+
   return (
     <StyledContainer>
-      <Paper elevation={2}>
-        <Stepper activeStep={1} alternativeLabel>
+      <Paper elevation={0}>
+        <Stepper activeStep={activeStep} alternativeLabel>
           {steps.map((label) => (
             <Step key={label}>
               <StepLabel>{label}</StepLabel>
             </Step>
           ))}
         </Stepper>
+        <Box>
+            {stepsComponent[activeStep]}
+        </Box>
       </Paper>
     </StyledContainer>
   );
