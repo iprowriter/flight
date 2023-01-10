75b5ad8050e4bd2fd35cc0bf576ad9a7c14ecd2b
iprowriter <obinnamartin72@gmail.com>
2023-01-10 23:45:31 +0100

    Redux #12

--- src/components/pricingPage_components/UserForm.tsx
+++ src/components/pricingPage_components/UserForm.tsx
@@ -7,7 +7,12 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { useDispatch } from "react-redux";
 import { userActiveStep } from "../../redux/slices/stepperStage";
-import { userFirstname, userLastname, userEmail, userAddress } from "../../redux/slices/userFormSlice";
+import {
+  userFirstname,
+  userLastname,
+  userEmail,
+  userAddress,
+} from "../../redux/slices/userFormSlice";
 import type { RootState } from "../../redux/store";
 import { useSelector } from "react-redux";
 import React from "react";
@@ -26,17 +31,17 @@ export function SmallCard() {
         <Typography variant="h3" color="info.main" gutterBottom align="center">
           Awesome!
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary"  align="center">
+        <Typography sx={{ mb: 1.5 }} color="text.secondary" align="center">
           You are almost ready to fly with us in the direction:
         </Typography>
         <Typography variant="h6" color="success" align="center">
-            Berlin - London 
+          Berlin - London
         </Typography>
         <Typography variant="h5" color="info.main" gutterBottom align="center">
           $120 (economy)
         </Typography>
         <Typography variant="body2" align="center">
-          Please complete the form below 
+          Please complete the form below
         </Typography>
       </CardContent>
     </Card>
@@ -49,25 +54,26 @@ export default function UserForm() {
   //Redux to increment activeStep used in Steps.tsx component.
   const dispatch = useDispatch();
 
-  let currentStep = useSelector((state: RootState) => state.stepperStage.activeStep)
-  
+  let currentStep = useSelector(
+    (state: RootState) => state.stepperStage.activeStep
+  );
+
   const incrementSteps = () => {
-    dispatch(userActiveStep(currentStep + 1))
-  }
+    dispatch(userActiveStep(currentStep + 1));
+  };
 
   //Redux to get and set values of User Input
-  const [firstname, setFirstname] = React.useState<string>("")
-  const [lastname, setLastname] = React.useState<string>("")
-  const [email, setEmail] = React.useState<string>("")
-  const [address, setAddress] = React.useState<string>("")
+  const [firstname, setFirstname] = React.useState<string>("");
+  const [lastname, setLastname] = React.useState<string>("");
+  const [email, setEmail] = React.useState<string>("");
+  const [address, setAddress] = React.useState<string>("");
 
-  React.useEffect(()=>{
-    console.log(firstname, lastname, email, address)
-    dispatch(userFirstname(firstname))
-    dispatch(userLastname(lastname))
-    dispatch(userEmail(email))
-    dispatch(userAddress(address))
-  },[firstname, lastname, email, address, dispatch])
+  React.useEffect(() => {
+    dispatch(userFirstname(firstname));
+    dispatch(userLastname(lastname));
+    dispatch(userEmail(email));
+    dispatch(userAddress(address));
+  }, [firstname, lastname, email, address, dispatch]);
 
   return (
     <StyledContainer>
@@ -95,21 +101,48 @@ export default function UserForm() {
                 <SmallCard />
               </Grid>
               <Grid item sx={{ width: "100%" }}>
-                <TextField fullWidth label="firstname" id="fullWidth" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+                <TextField
+                  fullWidth
+                  label="firstname"
+                  id="fullWidth"
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                />
               </Grid>
               <Grid item sx={{ width: "100%" }}>
-                <TextField fullWidth label="lastname" id="fullWidth" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+                <TextField
+                  fullWidth
+                  label="lastname"
+                  id="fullWidth"
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                />
               </Grid>
               <Grid item sx={{ width: "100%" }}>
-                <TextField fullWidth label="email" id="fullWidth" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <TextField
+                  fullWidth
+                  label="email"
+                  id="fullWidth"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </Grid>
               <Grid item sx={{ width: "100%" }}>
-                <TextField fullWidth label="address" id="fullWidth" value={address} onChange={(e) => setAddress(e.target.value)} />
+                <TextField
+                  fullWidth
+                  label="address"
+                  id="fullWidth"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
               </Grid>
               <Grid item sx={{ width: "100%" }}>
-                <Button variant="contained" color="info" 
-                fullWidth
-                onClick={incrementSteps}>
+                <Button
+                  variant="contained"
+                  color="info"
+                  fullWidth
+                  onClick={incrementSteps}
+                >
                   Continue to Checkout
                 </Button>
               </Grid>
